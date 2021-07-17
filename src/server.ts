0dfeb5c0743bcb3f14789bd3f9cd187af9205d9d
WMXPY <wm@mengw.io>
2021-07-17 13:25:41 -0500

    init implement server

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,36 @@
  * @description Socket
  */
 
+import * as HTTP from "http";
+import { server as WebsocketServer } from "websocket";
+
 export class SocketServer {
 
     public static create(): SocketServer {
         return new SocketServer();
     }
 
+    private _socketServer?: WebsocketServer;
+
     private _mounted: boolean;
 
     private constructor() {
 
         this._mounted = false;
+    }
+
+    public attach(server: HTTP.Server): this {
+
+        if (this._mounted) {
+            return this;
+        }
+
+        this._mounted = true;
+
+        this._socketServer = new WebsocketServer({
+            httpServer: server,
+            autoAcceptConnections: false,
+        });
+        return this;
     }
 }
