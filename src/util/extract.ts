d5fdd16504e61fcfa30875e22d60502b09c5ab7b
WMXPY <wm@mengw.io>
2021-07-17 17:16:44 -0500

    fix url

--- src/util/extract.ts
+++ src/util/extract.ts
@@ -10,13 +10,13 @@ import { ConnectionInformation, ConnectionURL } from "../declare";
 
 const extractResourceURL = (original: Url): ConnectionURL => {
 
-    return {
+    const url: ConnectionURL = {
 
         href: original.href,
         query: (typeof original.query === 'string'
             ? {}
             : {
-                ...original.query,
+                ...original.query as any,
             })
             ?? {},
 
@@ -31,6 +31,26 @@ const extractResourceURL = (original: Url): ConnectionURL => {
         slashes: original.slashes ?? false,
         port: original.port ?? undefined
     };
+
+    const entries: Array<[string, any]> = Object.entries(url);
+    return entries.reduce(
+        (
+            previous: ConnectionURL,
+            current: [string, any],
+        ) => {
+
+            if (typeof current[1] === 'undefined'
+                || current[1] === null) {
+                return previous;
+            }
+
+            return {
+                ...previous,
+                [current[0]]: current[1],
+            };
+        },
+        {} as any as ConnectionURL,
+    );
 };
 
 export const extractConnectionInformation = (request: WebsocketRequest): ConnectionInformation => {
