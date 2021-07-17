6d041cdcec6d0e4cbd711bb8612d76e4fde39f66
WMXPY <wm@mengw.io>
2021-07-17 17:10:24 -0500

    add declare conpressior

--- src/declare.ts
+++ src/declare.ts
@@ -4,9 +4,29 @@
  * @description Declare
  */
 
+export type ConnectionURL = {
+
+    readonly href: string;
+    readonly query: Record<string, string | string[]>;
+
+    readonly auth?: string;
+    readonly hash?: string;
+    readonly host?: string;
+    readonly hostname?: string;
+    readonly path?: string;
+    readonly pathname?: string;
+    readonly protocol?: string;
+    readonly search?: string;
+    readonly slashes?: boolean;
+    readonly port?: string;
+};
+
 export type ConnectionInformation = {
 
+    readonly url: ConnectionURL;
     readonly origin: string;
+    readonly userAgent: string;
+    readonly websocketVersion: number;
 };
 export type ConnectionEstablishRequirement = (information: ConnectionInformation) => boolean;
 export type OnConnectionCloseFunction = (reason: number, description: string) => void;
--- src/util/extract.ts
+++ src/util/extract.ts
@@ -3,13 +3,43 @@
  * @namespace Socket_Util
  * @description Extract
  */
+
+import { Url } from "url";
 import { request as WebsocketRequest } from "websocket";
-import { ConnectionInformation } from "../declare";
+import { ConnectionInformation, ConnectionURL } from "../declare";
+
+const extractResourceURL = (original: Url): ConnectionURL => {
+
+    return {
+
+        href: original.href,
+        query: (typeof original.query === 'string'
+            ? {}
+            : {
+                ...original.query,
+            })
+            ?? {},
+
+        auth: original.auth ?? undefined,
+        hash: original.hash ?? undefined,
+        host: original.host ?? undefined,
+        hostname: original.hostname ?? undefined,
+        path: original.path ?? undefined,
+        pathname: original.pathname ?? undefined,
+        protocol: original.protocol ?? undefined,
+        search: original.search ?? undefined,
+        slashes: original.slashes ?? false,
+        port: original.port ?? undefined
+    };
+};
 
 export const extractConnectionInformation = (request: WebsocketRequest): ConnectionInformation => {
 
     return {
 
+        url: extractResourceURL(request.resourceURL),
         origin: request.origin,
+        websocketVersion: request.webSocketVersion,
+        userAgent: request.httpRequest.headers["user-agent"] as string,
     };
 };
